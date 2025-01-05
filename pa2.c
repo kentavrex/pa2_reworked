8b9a2f47f406405be9d6a5be0449520f4ff9cf81
Ханов Амир Асадович <khanov@tochka.com>
2025-01-05 21:00:48 +0300

    after 60%

--- pa2.c
+++ pa2.c
@@ -82,56 +82,6 @@ void create_child_processes(int num_processes, int *balances, Pipe **pipes, FILE
     }
 }
 
-Process initialize_child(int i, int num_processes, int *balances, Pipe **pipes) {
-    return initialize_child_process(i, num_processes, balances, pipes);
-}
-
-void close_non_related_pipes(Process *child_proc, FILE *log_pipes) {
-    close_non_related_pipes(child_proc, log_pipes);
-}
-
-void send_start_message(Process *child_proc) {
-    send_message(child_proc, STARTED, NULL);
-}
-
-void log_process_start(int i, FILE *log_events, Process *child_proc) {
-    log_process_start(i, log_events, child_proc);
-}
-
-void check_received_all_started(Process *child_proc, int i) {
-    if (check_all_received(child_proc, STARTED) != 0) {
-        fprintf(stderr, "Error: Process %d failed to receive all STARTED messages\n", i);
-        exit(EXIT_FAILURE);
-    }
-}
-
-void log_received_all_started(FILE *log_events, int i) {
-    fprintf(log_events, log_received_all_started_fmt, get_physical_time(), i);
-}
-
-void perform_bank_operations(Process *child_proc, FILE *log_events) {
-    bank_operations(child_proc, log_events);
-}
-
-void close_outcoming_pipes(Process *child_proc, FILE *log_pipes) {
-    close_outcoming_pipes(child_proc, log_pipes);
-}
-
-void handle_child_process(local_id i, int num_processes, int *balances, Pipe **pipes, FILE *log_pipes, FILE *log_events) {
-    Process child_proc = initialize_child(i, num_processes, balances, pipes);
-
-    close_non_related_pipes(&child_proc, log_pipes);
-    send_start_message(&child_proc);
-    log_process_start(i, log_events, &child_proc);
-
-    check_received_all_started(&child_proc, i);
-    log_received_all_started(log_events, i);
-
-    perform_bank_operations(&child_proc, log_events);
-    close_outcoming_pipes(&child_proc, log_pipes);
-}
-
-
 Process initialize_child_process(local_id i, int num_processes, int *balances, Pipe **pipes) {
     Process child_proc = {
         .num_process = num_processes,
@@ -143,6 +93,56 @@ Process initialize_child_process(local_id i, int num_processes, int *balances, P
     };
     return child_proc;
 }
+
+Process initialize_child(local_id i, int num_processes, int *balances, Pipe **pipes) {
+    return initialize_child_process(i, num_processes, balances, pipes);
+}
+
+void close_non_related_pipes(Process *child_proc, FILE *log_pipes) {
+    close_non_related_pipes(child_proc, log_pipes);
+}
+
+void send_start_message(Process *child_proc) {
+    send_message(child_proc, STARTED, NULL);
+}
+
+void log_process_start_work(local_id i, FILE *log_events, Process *child_proc) {
+    log_process_start(i, log_events, child_proc);
+}
+
+void check_started_messages(Process *child_proc, local_id i) {
+    if (check_all_received(child_proc, STARTED) != 0) {
+        fprintf(stderr, "Error: Process %d failed to receive all STARTED messages\n", i);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void log_received_all_started(local_id i, FILE *log_events) {
+    fprintf(log_events, log_received_all_started_fmt, get_physical_time(), i);
+}
+
+void perform_bank_operations(Process *child_proc, FILE *log_events) {
+    bank_operations(child_proc, log_events);
+}
+
+void close_outgoing_pipes(Process *child_proc, FILE *log_pipes) {
+    close_outcoming_pipes(child_proc, log_pipes);
+}
+
+void handle_child_process(local_id i, int num_processes, int *balances, Pipe **pipes, FILE *log_pipes, FILE *log_events) {
+    Process child_proc = initialize_child(i, num_processes, balances, pipes);
+
+    close_non_related_pipes(&child_proc, log_pipes);
+    send_start_message(&child_proc);
+    log_process_start_work(i, log_events, &child_proc);
+
+    check_started_messages(&child_proc, i);
+    log_received_all_started(i, log_events);
+
+    perform_bank_operations(&child_proc, log_events);
+    close_outgoing_pipes(&child_proc, log_pipes);
+}
+
 
 void handle_parent_process(int num_processes, Pipe **pipes, FILE *log_pipes, FILE *log_events) {
     Process parent_proc = {
